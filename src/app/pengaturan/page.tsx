e2b659c1e35fbb1e0c157c7dd19f3c7d34ab66d0
Rifki Andrean <rifkiandrean@gmail.com>
2025-10-27 11:31:04 +0000

    masih loading terus di starting app

--- src/app/pengaturan/page.tsx
+++ src/app/pengaturan/page.tsx
@@ -271,7 +271,7 @@ export default function SettingsPage() {
     }
   };
 
-  if (isUserLoading) {
+  if (isUserLoading || (usersLoading && !userAccounts)) {
       return (
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
             <div className="flex items-center justify-between">
